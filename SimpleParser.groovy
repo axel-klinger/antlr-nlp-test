e5a60ca6edf91bd266cd68857bad6bebff46c247
Axel Klinger <axel.klinger@web.de>
2018-08-30 06:45:08 +0200

    Start structuring methods

--- SimpleParser.groovy
+++ SimpleParser.groovy
@@ -7,17 +7,18 @@ println text
 
 // sentenceize
 println "1. SENTENCIZED"
-def sentences = text.split('\\.')
-sentences.each { s ->
-  s = s.trim()
-  println s
-}
+println sentenceize(text)
+// def sentences = text.split('\\.')
+// sentences.each { s ->
+//   s = s.trim()
+//   println s
+// }
 
 // each sentence tokenize
 println "2. TOKENIZED"
 sentences.each { s ->
-  def words = s.trim().split(' ')
-  println words.join(' | ')
+  // def words = s.trim().split(' ')
+  println tokenize(s) //.join(' | ')
 }
 
 // tag each sentence
@@ -28,23 +29,44 @@ sentences.each { s ->
 }
 println "Ein [ART_S_M_N, ART_S_M_A, ...] Graph [NN_S_M_N] ...\n"
 
-println "4. PARSE ALL POSSIBLE COMINATIONS AND PRINT ALL VALID SENTENCSES"
+println "4. CREATE ALL POSSIBLE SENTENCES"
+
+println "5. PARSE ALL POSSIBLE COMINATIONS AND PRINT ALL VALID SENTENCSES"
 println "Ein [ARTUB] Graph [NN] ist [VAFIN] ein [ARTUB] ...\n"
 
-println "5. OUTPUT"
+println "6. OUTPUT"
 println "(TEXT (SENT (NP) ...) (SENT (NP) ...))\n"
 
-// DEBUGGING
+// DEBUGGING => done by TreeListener impls
 // * color text in HTML with found tags
 // * color text in HTML with unique parse results
 
+def sentenceize(text) {
+  return text.split('\\.')
+}
+
+def tokenize(sentence) {
+  return sentence.trim().split(' ')
+}
+
+def tag(token) {
+  token.each { t ->
+    token += dictionary.get(t)
+  }
+  return token
+}
+
+def parse(tokenStream) {
+  return
+}
+
 def loadDictionary(language) {
   def dictionaryText = new File('dictionary-' + language + '.csv').text
-  def dictionary = [] as Map
+  def dictionary = [:]
   dictionaryText.eachLine { l ->
     def entry = l.split(';')
     if (entry.size() > 1) {
-      // dictionary.put(entry[0], entry[1])
+      dictionary.put(entry[0], entry[1])
     }
   }
   return dictionary
