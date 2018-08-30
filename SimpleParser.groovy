110ced654e48401bfd38e8c42a6de84e27de0a68
Axel Klinger <axel.klinger@web.de>
2018-08-30 22:19:25 +0200

    Update SimpleParser.groovy

--- SimpleParser.groovy
+++ SimpleParser.groovy
@@ -7,57 +7,109 @@ println text
 
 // sentenceize
 println "1. SENTENCIZED"
-println sentenceize(text)
-// def sentences = text.split('\\.')
-// sentences.each { s ->
-//   s = s.trim()
-//   println s
-// }
+def sentences = sentenceize(text)
+println sentences
 
-// each sentence tokenize
-println "2. TOKENIZED"
 sentences.each { s ->
-  // def words = s.trim().split(' ')
-  println tokenize(s) //.join(' | ')
+  println "a) TOKENIZE"
+  def words = tokenize(s) //.join(' | ')
+  println words
+
+  println "b) TAG"
+  def altSentences = tag(s,dictionary)
+  altSentences.each { x ->
+    println x
+  }
+
+  println "c) PARSE ALL POSSIBLE COMINATIONS AND PRINT ALL VALID SENTENCSES"
+  // def parsedSentences = parse(altSentences)
+  // parsedSentences.each { s ->
+  //   println s
+
 }
-
-// tag each sentence
-println "3. TAGGED => TODO: prepare word form dictionary (wiktionary)"
-sentences.each { s ->
-  def words = s.trim().split(' ')
-  // println words.join(' | ')
-}
-println "Ein [ART_S_M_N, ART_S_M_A, ...] Graph [NN_S_M_N] ...\n"
-
-println "4. CREATE ALL POSSIBLE SENTENCES"
-
-println "5. PARSE ALL POSSIBLE COMINATIONS AND PRINT ALL VALID SENTENCSES"
-println "Ein [ARTUB] Graph [NN] ist [VAFIN] ein [ARTUB] ...\n"
-
-println "6. OUTPUT"
-println "(TEXT (SENT (NP) ...) (SENT (NP) ...))\n"
 
 // DEBUGGING => done by TreeListener impls
 // * color text in HTML with found tags
 // * color text in HTML with unique parse results
 
 def sentenceize(text) {
-  return text.split('\\.')
+  def sentences = text.split('\\.')
+  def newSentences = [] as List
+  sentences.each { s ->
+    def sent = s.trim()
+    if (sent.size() > 0) {
+      newSentences.add(sent)
+    }
+  }
+  return newSentences
 }
 
 def tokenize(sentence) {
   return sentence.trim().split(' ')
 }
 
-def tag(token) {
-  token.each { t ->
-    token += dictionary.get(t)
+// "ich bin" -> "[ich_PRPERS bin_VA, ich_XXX bin_VA]"
+// dict + sent -> [sa, sb, sc]
+def tag(sentence, dictionary) {
+  def altSentences = [] as List
+  def words = tokenize(sentence)
+  def taggedWords = []
+  // für jedes Wort x Worte mit Form erzeugen
+  words.each { w ->
+    // println w
+    def wfs = dictionary.get(w).split(',')  // [ART,ADV]
+    def taggedWord = []
+    wfs.each { wf ->
+      taggedWord.add(w + '_' + wf.split(':')[0])
+    }
+    taggedWords.add(taggedWord)
   }
-  return token
+  // ... satz erzeugen ...
+  def counterForAllWords = new Integer[taggedWords.size()]
+  taggedWords.eachWithIndex { t,i ->
+    counterForAllWords[i] = 0
+  }
+
+  // 18x (n1 x n2 x n3 x ... x nX) Satz aufbauen ...
+  def newSentence = ''
+
+/*
+  sx
+  w1.each form w1x
+    wordindex ++
+    sx += w1x + rest(w wordIndex)
+    wordindex --
+
+  rest(nextTaggedWord) {
+    if (wordindex = taggedwords.size()) {
+      return
+    }
+    ...
+  }
+
+*/
+
+
+
+
+  // feedSentence
+    // fill and add new sentence!
+  println taggedWords
+
+
+  // for (int i = 0; i<words.size(), i++) {
+  //   taggedWords[i].each { t ->
+  //
+  //   }
+  //
+  // }
+
+
+  return altSentences // als Strings
 }
 
-def parse(tokenStream) {
-  return
+def parse(altSentences) {
+  return altSentences
 }
 
 def loadDictionary(language) {
